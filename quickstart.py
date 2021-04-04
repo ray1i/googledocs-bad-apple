c82cd84fd132f888531c29796f4619f23786fe6a
Ray Li <43012140+ray1i@users.noreply.github.com>
2021-04-04 16:25:34 -0400

    Add files via upload

---
+++ quickstart.py
@@ -0,0 +1,88 @@
+from __future__ import print_function
+import os.path
+from time import sleep
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+from vidtest import getframes
+
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+DOCUMENT_ID = '1HbI9540qtaDd0W13Y9typazBgutqB08j7-E_Zm2OTEQ'
+
+def main():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('docs', 'v1', credentials=creds)
+
+    document = service.documents().get(documentId=DOCUMENT_ID).execute()
+
+    def createdoc():
+        title = 'My Document'
+        body = {
+            'title': title
+        }
+        doc = service.documents().create(body=body).execute()
+        print('Created document with title:', doc.get('title'))
+
+    def createrange():
+        # Create range for video
+        request = [
+            {
+                'createNamedRange': {
+                    'name': 'main',
+                    'range': {
+                        'startIndex': 1,
+                        'endIndex': 2,
+                    }
+                }
+            },
+        ]
+        service.documents().batchUpdate(documentId=DOCUMENT_ID, body={'requests': request}).execute()
+
+    def deleteallranges():
+        request = [
+            {
+                'deleteNamedRange': {
+                    'name': 'main'
+                }
+            },
+        ]
+        service.documents().batchUpdate(documentId=DOCUMENT_ID, body={'requests': request}).execute()
+        print('remaining ranges:')
+        print(document.get('namedRanges', {}).get('main'))
+
+    request = [
+        {
+            'replaceNamedRangeContent': {
+                'text': 'youshouldntseethis',
+                'namedRangeName': 'main'
+            }
+        },
+    ]
+
+    frames = getframes('badapple.mp4', 10)
+
+    for i in range(len(frames)):
+        request[0]['replaceNamedRangeContent']['text'] = frames[i]
+        service.documents().batchUpdate(documentId=DOCUMENT_ID, body={'requests': request}).execute()
+
+if __name__ == '__main__':
+    main()
---
+++ vidtest.py
@@ -0,0 +1,80 @@
+from PIL import Image
+import cv2
+import os
+
+#"▖▗▘▙▚▛▜▝▞▟	▌▐▀▄"
+#chars = " !?@#"
+
+def convertimagefromurl(imageurl):
+    chars = " ░▒▓█"
+    m = int(255/4)
+
+    image = Image.open(imageurl).convert('L')
+    image = image.resize((80, 30))
+    imagelist = list(image.getdata())
+
+    res = ''
+
+    for i in range(len(imagelist)):
+        if i%80 == 0:
+            res += '\n'
+        res += chars[round(imagelist[i]/m)]
+
+    return res
+
+def convertimage(image):
+    chars = " ░▒▓█"
+    m = int(255/4)
+
+    image = image.convert('L')
+    image = image.resize((80, 30))
+    imagelist = list(image.getdata())
+
+    res = ''
+
+    for i in range(len(imagelist)):
+        if i%80 == 0:
+            res += '\n'
+        res += chars[round(imagelist[i]/m)]
+
+    return res
+
+def getframes(vidurl, fps):
+    vid = cv2.VideoCapture(vidurl)
+
+    frames = []
+
+    curr = 0
+    while True:
+        ret, frame = vid.read()
+        if ret:
+            if curr % (30/fps) == 0:
+                frames.append(convertimage(Image.fromarray(frame)))
+        else:
+            print('done.')
+            break
+        curr += 1
+
+    vid.release()
+    cv2.destroyAllWindows()
+
+    return frames
+
+'''
+    image = Image.open('testimage.png').convert('L')
+    image = image.resize((80, 30))
+    imagelist = list(image.getdata())
+
+    f = open('test.txt', 'w')
+    f.write('')
+    f.close()
+
+    f = open('test.txt', 'a', encoding='utf-8')
+
+    for i in range(len(imagelist)):
+        if i%80 == 0:
+            f.write('\n')
+        f.write(chars[round(imagelist[i]/m)])
+
+    f.close()
+'''
